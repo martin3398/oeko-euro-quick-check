39068ffb066ee89b4edec530ce4aea140e064dfb
Martin Ziegler <martin.ziegler3398@gmail.com>
2024-09-29 21:12:25 +0200

    Added hints

--- src/index.js
+++ src/index.js
@@ -35,27 +35,36 @@ function checkCriteria() {
 }
 
 function isRejected(participants, cars, days, showHints) {
+    let rejected = false;
+
     const transportFlugzeug = document.getElementById('transport7').checked;
-    if (transportFlugzeug) return true;
+    if (transportFlugzeug) rejected = true;
+    setRejectedHint('transport7', transportFlugzeug, showHints);
 
     const plasticAvoided = document.getElementById('waste1').checked;
-    if (!plasticAvoided) return true;
+    if (!plasticAvoided) rejected = true;
+    setRejectedHint('waste1', !plasticAvoided, showHints);
 
     const wasteSeparated = document.getElementById('waste5').checked;
-    if (!wasteSeparated) return true;
+    if (!wasteSeparated) rejected = true;
+    setRejectedHint('waste5', !wasteSeparated, showHints);
 
     const allowedCars = Math.ceil(participants / 10) * 3;
-    if (cars > allowedCars) return true;
+    if (cars > allowedCars) rejected = true;
+    setRejectedHint('carCount', cars > allowedCars, showHints);
 
     const isVegan = document.getElementById('food1').checked;
     const isVegetarian = document.getElementById('food2').checked;
     const meatMeals = document.getElementById('meatMeals').value;
-    if (!isVegan && !isVegetarian && meatMeals >= days) return true;
+    if (!isVegan && !isVegetarian && meatMeals >= days) rejected = true;
+    setRejectedHint('meatMeals', !isVegan && !isVegetarian && meatMeals >= days, showHints);
 
-    return false;
+    return rejected;
 }
 
 function isAccepted(showHints) {
+    let accepted = true;
+
     const participants = parseInt(document.getElementById('participants').value, 10);
     const cars = parseInt(document.getElementById('carCount').value, 10);
     const days = parseInt(document.getElementById('days').value, 10);
@@ -81,25 +90,56 @@ function isAccepted(showHints) {
     const regionalChecked = document.getElementById('food7').checked;
 
     const ecoMeasuresChecked = document.getElementById('theme1').checked;
-    if (autoChecked) return false;
-    if (reisebusChecked && busEmptyChecked) return false;
+    if (autoChecked) accepted = false;
+    setContactHint('transport6', autoChecked, showHints);
+    if (reisebusChecked && busEmptyChecked) accepted = false;
+    setContactHint('busEmpty', reisebusChecked && busEmptyChecked, showHints);
 
     const allowedCars = Math.ceil(participants / 50);
-    if (cars > allowedCars) return false;
-    if (cars >= 1 && carDistance > 500) return false;
-    if (!bottlesChecked) return false;
-    if (!lunchboxesChecked) return false;
-    if (!leftoversChecked) return false;
-    if (days < 3 && meatMeals > 0) return false;
-    if (days === 3 && meatMeals > 1) return false
-    if (days > 3 && meatMeals > Math.floor(days / 3)) return false;
-    if (meatChecked && meatMeals > 0 && !bioMeatChecked) return false;
-    if (meatChecked && meatMeals > 0 && !mscFishChecked) return false;
-    if (!fairTradeChecked) return false;
-    if (!regionalChecked) return false;
-    if (!ecoMeasuresChecked) return false;
+    if (cars > allowedCars) accepted = false;
+    setContactHint('carCount', cars > allowedCars, showHints);
+    if (cars >= 1 && carDistance > 500) accepted = false;
+    setContactHint('carDistance', cars >= 1 && carDistance > 500, showHints);
+    if (!bottlesChecked) accepted = false;
+    setContactHint('waste2', !bottlesChecked, showHints);
+    if (!lunchboxesChecked) accepted = false;
+    setContactHint('waste3', !lunchboxesChecked, showHints);
+    if (!leftoversChecked) accepted = false;
+    setContactHint('waste4', !leftoversChecked, showHints);
+    if (days < 3 && meatMeals > 0) accepted = false;
+    if (days < 3) setContactHint('meatMeals', meatMeals > 0, showHints);
+    if (days === 3 && meatMeals > 1) accepted = false;
+    if (days === 3) setContactHint('meatMeals', meatMeals > 1, showHints);
+    if (days > 3 && meatMeals > Math.floor(days / 3)) accepted = false;
+    if (days > 3) setContactHint('meatMeals', meatMeals > Math.floor(days / 3), showHints);
+    if (meatChecked && meatMeals > 0 && !bioMeatChecked) accepted = false;
+    if (meatChecked) setContactHint('food4', meatChecked && meatMeals > 0 && !bioMeatChecked, showHints);
+    if (meatChecked && meatMeals > 0 && !mscFishChecked) accepted = false;
+    if (meatChecked) setContactHint('food5', meatChecked && meatMeals > 0 && !mscFishChecked, showHints);
+    if (!fairTradeChecked) accepted = false;
+    setContactHint('food6', !fairTradeChecked, showHints);
+    if (!regionalChecked) accepted = false;
+    setContactHint('food7', !regionalChecked, showHints);
+    if (!ecoMeasuresChecked) accepted = false;
+    setContactHint('theme1', !ecoMeasuresChecked, showHints);
 
-    return true;
+    return accepted;
+}
+
+function setRejectedHint(id, showHint, showHints) {
+    if (showHint && showHints) {
+        document.getElementById(id).parentElement.classList.add('hintRejected');
+    } else {
+        document.getElementById(id).parentElement.classList.remove('hintRejected');
+    }
+}
+
+function setContactHint(id, showHint, showHints) {
+    if (showHint && showHints) {
+        document.getElementById(id).parentElement.classList.add('hintContact');
+    } else {
+        document.getElementById(id).parentElement.classList.remove('hintContact');
+    }
 }
 
 function toggleMeatSection() {
